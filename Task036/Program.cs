b0499363b42fa82cc4260b3d75110bb2bb4a0c25
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-04 20:34:26 +0300

    Add task 36

---
+++ Task036/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.[3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
+
+int [] mass = new int [4];
+int summ = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass [i] = new Random().Next(0, 30);
+}
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.WriteLine($"{mass[i]} ");
+}
+for (int i = 1; i < mass.Length; i+=2)
+{
+    summ = summ + mass[i];
+
+}
+Console.WriteLine($"Cумма нечетных позиций {summ}");
