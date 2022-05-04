7efbd49d3c03170634193089d4893806f40d31c4
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-04 20:28:43 +0300

    Add task 34

---
+++ Task034/Program.cs
@@ -0,0 +1,28 @@
+﻿/// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+int [] mass = new int [4];
+int summOdd = 0;
+int summEven = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass [i] = new Random().Next(100, 999);
+}
+
+for (int i = 0; i < mass.Length; i++ )
+{
+Console.WriteLine($"{mass[i]} ");
+}
+
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] % 2 == 1)                                 
+    { 
+       summOdd = summOdd + 1;
+    }
+else
+{
+ summEven = summEven + 1;
+} 
+}
+Console.WriteLine($"Колличество четных чисел равно {summEven}");
